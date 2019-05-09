a989da1733ff41b6f3e368a07fca6555a69995f0
Willian Alves <wba.alves@gmail.com>
2019-05-08 22:28:42 -0300

    modify lookup to create endpoint for a given ip

--- FSClient.java
+++ FSClient.java
@@ -5,8 +5,8 @@ public class FSClient {
 
     public static void main(String[] argv) {
         try {
-            fileService = (FSInterface) Naming.lookup("//localhost/FileService");
-            ServiceProperties result = new FSExecutor(fileService).execute(argv[0], argv);
+            fileService = (FSInterface) Naming.lookup(String.format("//%s/FileService", argv[0]));
+            ServiceProperties result = new FSExecutor(fileService).execute(argv[1], argv);
             ResultPrinter printer = ResultPrinter.getInstance();
             printer.showResult(result);
         } catch (Exception e) {
